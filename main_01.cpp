a45b5bae482175d755888214b639551cfe5547ed
Denis <denis_bourqui@hotmail.com>
2019-05-02 15:50:42 +0200

    codecheck 1 pass

--- main_01.cpp
+++ main_01.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main() {
+  Int a; // a = 0
+  Int b(5); // b = 5
 
+  Int c(b); // c = 5 et b = 5
+  Int d(move(c)); // d = 5 et c = -1
 
+  b = d; // b = 5 et d = 5
+  a = move(d); // a = 5 et d = -2
 }
